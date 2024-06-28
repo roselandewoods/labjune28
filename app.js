ae116a0913ba3dbbed61c4c0c649046382d770d8
roselandewoods <roselandewoods@gmail.com>
2024-06-28 16:58:25 -0400

    lab

--- app.js
+++ app.js
@@ -10,28 +10,42 @@
 // The Solar System, part 1
  
 
-const solarSystem = [
-{ name: "Mercury", ringSystem: false, moons: [] },
-{ name: "Venus", ringSystem: false, moons: [] },
-{ name: "Earth", ringSystem: false, moons: ["The Moon"] },
-{ name: "Mars", ringSystem: false, moons: ["Phobos", "Deimos"] },
-{ name: "Jupiter", ringSystem: true, moons: ["Europa", "Ganymede", "Io", "Callisto"] },
-{ name: "Saturn", ringSystem: true, moons: ["Titan", "Enceladus", "Rhea", "Mimas"] },
-{ name: "Uranus", ringSystem: true, moons: ["Miranda", "Titania", "Ariel", "Umbriel"] },
-{ name: "Neptune", ringSystem: true, moons: ["Triton", "Nereid"] }
-];
+
  
 
 // 1. Print the array of Jupiter's moons to the console (no for loop, just print the entire array).
 
 
+const solarSystem = [
+    { name: "Mercury", ringSystem: false, moons: [] },
+    { name: "Venus", ringSystem: false, moons: [] },
+    { name: "Earth", ringSystem: false, moons: ["The Moon"] },
+    { name: "Mars", ringSystem: false, moons: ["Phobos", "Deimos"] },
+    { name: "Jupiter", ringSystem: true, moons: ["Europa", "Ganymede", "Io", "Callisto"] },
+    { name: "Saturn", ringSystem: true, moons: ["Titan", "Enceladus", "Rhea", "Mimas"] },
+    { name: "Uranus", ringSystem: true, moons: ["Miranda", "Titania", "Ariel", "Umbriel"] },
+    { name: "Neptune", ringSystem: true, moons: ["Triton", "Nereid"] }
+    ];
+
+
+
+    console.log(solarSystem[4].moons);
+
+
+
+
+
 
 // 2. Print the name of Neptune's moon "Nereid" to the console.
+
+console.log(solarSystem[7].moons);
+
 
 
 
 // 3. Add a new moon called "Endor" to Venus' moons array.
 
+solarSystem[1] 
 
 
 // 4. Add a Pluto object to the solarSystem array using .push. The object should contain Pluto's name, ringSystem boolean, and moons array (which includes "Charon").
